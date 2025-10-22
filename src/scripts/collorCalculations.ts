bbb818a0f8e29f4527196cb65cadef4b4015b99e
Melanie Kuhles <melanie.kuhles@oder-spree-consulting.de>
2025-10-22 23:41:50 +0200

    tidy up and ajust circle size

--- src/scripts/collorCalculations.ts
+++ src/scripts/collorCalculations.ts
@@ -1,13 +1,3 @@
-// {#E7E0CF, #D7D6C2, #C6CCB6, #ACBFAF, #8FB1AA, #779EAB, #628AA3, #4F7392, #3E5C7A});
-
-
-
-// von sehr hell nach navy (muted)
-export const BLUE_SCALE = [
-  '#E3EAF2', '#D3E0EE', '#C1D4E8',
-  '#A9C4DE', '#90B3D3', '#789FC6',
-  '#608AB7', '#4E74A1', '#3E5F88'
-];
 
 export const COLOR_SCALE = [
     '#E7E0CF', '#D7D6C2', '#C6CCB6',
