740428bb83de9cdf5bd769db0b71f7badeff8e6c
Melanie Kuhles <melanie.kuhles@oder-spree-consulting.de>
2026-02-04 22:33:05 +0100

    Remove unused vite-imagetools

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import { imagetools } from 'vite-imagetools';
 import mdx from "@astrojs/mdx";
 
 import tailwindcss from "@tailwindcss/vite";
@@ -17,7 +16,7 @@ export default defineConfig({
   },
   integrations: [mdx()],
   vite: {
-    plugins: [imagetools(), tailwindcss()],
+    plugins: [tailwindcss()],
     css: {
       transformer: "postcss",
     },
